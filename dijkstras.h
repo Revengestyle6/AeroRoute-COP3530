b28bb6d7f26f5c8d0a11d29b382653e9dc690667
Revengestyle6 <ctgpghosts@gmail.com>
2026-03-26 18:42:21 -0400

    separated dijkstras.h into 2 files

--- dijkstras.h
+++ dijkstras.h
@@ -7,22 +7,8 @@
 //maybe create a class for flight sim that holds all the routes
 //dont load it everytime
 
-std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> adjacency_list(std::map<std::pair<std::string, std::string>, double> routes){
-    std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> adj_list;
-    //route = key
-    for (auto& route : routes){
-        adj_list[route.first.first].push_back(make_pair(route.first.second, route.second));
-    }
-    return adj_list;
-}
+std::unordered_map<std::string, std::vector<std::pair<std::string, int>>> adjacency_list(std::map<std::pair<std::string, std::string>, double> routes);
 
-//dijkstras runs off of adjacency graph
-std::vector<int> dijkstras(const std::string& source, const std::string& destination, std::map<std::pair<std::string, std::string>, double> routes){
-    //distance array for shortest known distance from source
-    //predecessor array for previous nodes in shortest path
-    //visited set for nodes who's shortest distances have been finalized
-    //another set for nodes that still need to be finalized/ or priority queue
-
-
-}
+// dijkstra runs off of adjacency graph. Returns a vector of node indices for now.
+std::vector<int> dijkstras(const std::string& source, const std::string& destination, std::map<std::pair<std::string, std::string>, double> routes);
 #endif
